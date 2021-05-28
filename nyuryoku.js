725acf3d63239854d350e9d009f330a0d2db665d
eonki <82816639+eonki@users.noreply.github.com>
2021-05-28 12:05:17 +0900

    Create nyuryoku.js

---
+++ nyuryoku.js
@@ -0,0 +1,22 @@
+window.onload = function (){
+
+calcseats();
+
+
+};
+
+function calcseats() {
+  const seatsretsu = document.getElementById("retsu").selectedIndex +1;
+  const seatsgyo = document.getElementById("gyo").selectedIndex +1;
+  const maxseats = seatsgyo * seatsretsu;
+  const seatcheck = document.getElementsByName("seatcheck");
+  let selectedseats = 0;
+  for (let i=0; i<maxseats; i++){
+        if (seatcheck[i].checked){
+          selectedseats++;
+      }
+  }
+
+  document.getElementById("selectedseats").innerHTML = selectedseats;
+  document.getElementById("maxseats").innerHTML = maxseats;
+};
