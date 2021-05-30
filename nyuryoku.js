684c9384966a5b99ada6415edd90e882819e031c
eonki <82816639+eonki@users.noreply.github.com>
2021-05-30 13:19:38 +0900

    Update nyuryoku.js

--- nyuryoku.js
+++ nyuryoku.js
@@ -9,6 +9,7 @@ calcseats();
 function calcseats() {
   const seatsretsu = document.getElementById("retsu").selectedIndex +1;
   const seatsgyo = document.getElementById("gyo").selectedIndex +1;
+  const maxseats = seatsgyo * seatsretsu;
 
   const seatcheck = document.getElementsByName("seatcheck");
   let selectedseats = 0;
