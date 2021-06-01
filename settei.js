9cdd5c6cc0bb5104cc1883aaf97a791e5e1debeb
emiri <83271293+emiringo1107@users.noreply.github.com>
2021-06-02 00:10:59 +0900

    Create settei.js

---
+++ settei.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $(".button").on("click", function() {
+      $("div.modal").addClass("is-active");
+    })
+
+    $("#close, div.modal-background").on("click", function() {
+      $("div.modal").removeClass("is-active");
+    })
+  });
+window.onload = function (){
+
+dispseats();
+calcseats();
+
+};
